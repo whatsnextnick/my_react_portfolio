03932860a745af5ff2a044feafef3498ffb43dc9
Jawascript <n.jmonplaisir@gmail.com>
2023-06-09 00:24:11 -0400

    adding footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright &copy; Nicholas Monplaisir 2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
