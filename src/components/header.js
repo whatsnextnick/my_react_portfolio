77ecbf33b46ada89d85bae179444ee690d1edf6e
Jawascript <n.jmonplaisir@gmail.com>
2023-06-09 00:20:45 -0400

    adding header components and social/contact info

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Nicholas Monplaisir</h1>
+      <nav>
+        <a href="https://github.com/whatsnextnick" target="_blank" rel="noopener noreferrer">GitHub</a> |
+        <a href="https://www.linkedin.com/in/nmonplaisir" target="_blank" rel="noopener noreferrer">LinkedIn</a> |
+        <a href="mailto:n.jmonplaisir@gmail.com">Email</a> |
+        <a href="tel:9546651456">Phone</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
